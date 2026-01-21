5a0b5ba04d2bdfc1de07d346f3ab60bfe44cfa15
Deep_Thinker <145069547+DANUTSHELL@users.noreply.github.com>
2026-01-21 14:23:04 +0000

    Creation of Create, Insert and Close functions

--- CRUD.py
+++ CRUD.py
@@ -1,1 +1,46 @@
+import sqlite3
 
+def parcours_listes(L1,L2):
+    attributs = ""
+    taille = len(L1)
+
+    for i in range(taille):
+        attributs = attributs + f"{L1[i]} {L2[i]}"
+
+        if i< taille - 1:
+            attributs = attributs + ", "
+    return attributs
+
+def parcours_valeurs(L):
+    nouveau = ""
+    taille = len(L)
+
+    for i in range(taille):
+        if type(L[i]) == str:
+            nouveau = nouveau + f"'{L[i]}'"
+        else:
+            nouveau = nouveau + f"{L[i]}"
+
+        if i< taille - 1:
+            nouveau = nouveau + ", "
+    return nouveau
+
+class RequetesSQL :
+    def __init__(self, baseDB):
+        self.con = sqlite3.connect(baseDB)
+        self.cur = self.con.cursor()
+
+    def CreateTable(self, nomTable, listeAttributs, typeAttributs, PrimaryKey):
+        self.cur.execute(f"""CREATE TABLE IF NOT EXISTS {nomTable}(
+                         {parcours_listes(listeAttributs, typeAttributs)},PRIMARY KEY ({PrimaryKey})
+                         )""")
+        self.con.commit()
+    
+    def Insert(self, nomTable, listeValeurs):
+        self.cur.execute(f"""INSERT INTO {nomTable} VALUES(
+                         {parcours_valeurs(listeValeurs)}
+                         )""")
+        self.con.commit()
+
+    def CloseDB(self):
+        self.con.close()
