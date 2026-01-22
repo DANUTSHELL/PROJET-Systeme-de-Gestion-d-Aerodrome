cf0e82878aa7b35cdc1b2af36553886b6ca519a5
Deep_Thinker <145069547+DANUTSHELL@users.noreply.github.com>
2026-01-22 18:10:10 +0000

    J'ai enlevé la partie index section qui ne sert plus à rien après avoir enlevé constraints

--- DB_Creation.py
+++ DB_Creation.py
@@ -7,7 +7,7 @@ cur.executescript("""
     create table Compte (
          identifiant varchar(255) not null,
          Mot_de_passe varchar(255) not null,
-         constraint ID_Compte_ID primary key (identifiant)
+         primary key (identifiant)
     );
 
     create table AgentExploitation (
@@ -17,7 +17,7 @@ cur.executescript("""
          Adresse varchar(255) not null,
          Telephone varchar(13) not null,
          identifiant varchar(255) not null,
-         constraint ID_AgentExploitation_ID primary key (id_agent),
+         primary key (id_agent)
          foreign key (identifiant) references Compte(identifiant)
     );
 
@@ -28,7 +28,7 @@ cur.executescript("""
          Adresse varchar(255) not null,
          Telephone varchar(13) not null,
          identifiant varchar(255) not null,
-         constraint ID_Gestionnaire_Aerodrome_ID primary key (id_gestionnaire),
+         primary key (id_gestionnaire),
          foreign key (identifiant) references Compte(identifiant)
     );
 
@@ -39,7 +39,7 @@ cur.executescript("""
          Adresse varchar(255) not null,
          Telephone varchar(13) not null,
          identifiant varchar(255) not null,
-         constraint ID_Pilote_ID primary key (id_pilote),
+         primary key (id_pilote),
          foreign key (identifiant) references Compte(identifiant)
     );
 
@@ -48,7 +48,7 @@ cur.executescript("""
          TypeAvion varchar(20) not null,
          Capacite_carburant numeric(3) not null,
          id_pilote numeric(100) not null,
-         constraint ID_Avion_ID primary key (id_avion),
+         primary key (id_avion),
          foreign key (id_pilote) references Pilote(id_pilote)
     );
 
@@ -56,7 +56,7 @@ cur.executescript("""
          Type_carburant varchar(30) not null,
          prix_litre float(10) not null,
          Quantite_max numeric(10) not null,
-         constraint ID_Carburant_ID primary key (Type_carburant)
+         primary key (Type_carburant)
     );
 
     create table Hangar (
@@ -65,21 +65,21 @@ cur.executescript("""
          Prix_jour numeric(10) not null,
          Prix_semaine numeric(10) not null,
          Prix_mois numeric(10) not null,
-         constraint ID_Hangar_ID primary key (id_hangar)
+         primary key (id_hangar)
     );
 
     create table Parking (
          id_parking numeric(100) not null,
          Taille varchar(10) not null,
          Prix char(1) not null,
-         constraint ID_Parking_ID primary key (id_parking)
+         primary key (id_parking)
     );
     
     create table Vol (
          id_vol numeric(100) not null,
          Heure_decollage varchar(10) not null,
          Heure_atterissage varchar(10) not null,
-         constraint ID_Vol_ID primary key (id_vol)
+         primary key (id_vol)
     );
 
     create table Facture (
@@ -89,7 +89,7 @@ cur.executescript("""
          Recette_annee numeric(100) not null,
          Prix_Total numeric(100) not null,
          id_agent numeric(100) not null,
-         constraint ID_Facture_ID primary key (id_facture),
+         primary key (id_facture),
          foreign key (id_agent) references AgentExploitation(id_agent)
     );
 
@@ -102,7 +102,7 @@ cur.executescript("""
          id_avion numeric(100) not null,
          id_parking numeric(100),
          id_facture numeric(100) not null,
-         constraint ID_Reservation_ID primary key (id_reservation),
+         primary key (id_reservation),
          foreign key (id_vol) references Vol(id_vol),
          foreign key (id_avion) references Avion(id_avion),
          foreign key (id_parking) references Parking(id_parking),
@@ -113,8 +113,7 @@ cur.executescript("""
          num_emplacement numeric(100) not null,
          id_reservation numeric(100) not null,
          id_hangar numeric(100) not null,
-         constraint ID_Emplacement_ID primary key (num_emplacement),
-         constraint FKLouer_ID unique (id_reservation),
+         primary key (num_emplacement),
          foreign key (id_hangar) references Hangar(id_hangar),
          foreign key (id_reservation) references Reservation(id_reservation)
     );
@@ -123,7 +122,7 @@ cur.executescript("""
          id_agent numeric(100) not null,
          id_pilote numeric(100) not null,
          Texte varchar(255) not null,
-         constraint ID_Message_ID primary key (id_agent, id_pilote),
+         primary key (id_agent, id_pilote),
          foreign key (id_pilote) references Pilote(id_pilote),
          foreign key (id_agent) references AgentExploitation(id_agent)
     );
@@ -132,41 +131,10 @@ cur.executescript("""
          id_reservation numeric(100) not null,
          Quantite numeric(10) not null,
          Type_carburant varchar(30) not null,
-         constraint FKRem_Res_ID primary key (id_reservation),
+         primary key (id_reservation),
          foreign key (id_reservation) references Reservation(id_reservation),
          foreign key (Type_carburant) references Carburant(Type_carburant)
     );
-
-    -- Index Section
-    -- _____________ 
-
-    create unique index ID_AgentExploitation_IND on AgentExploitation (id_agent);
-    create index FKAuthentifier_Agent_IND on AgentExploitation (identifiant);
-    create unique index ID_Avion_IND on Avion (id_avion);
-    create index FKPosseder_IND on Avion (id_pilote);
-    create unique index ID_Carburant_IND on Carburant (Type_carburant);
-    create unique index ID_Compte_IND on Compte (identifiant);
-    create unique index ID_Emplacement_IND on Emplacement (num_emplacement);
-    create index FKSituer_IND on Emplacement (id_hangar);
-    create unique index FKLouer_IND on Emplacement (id_reservation);
-    create unique index ID_Facture_IND on Facture (id_facture);
-    create index FKCreer_IND on Facture (id_agent);
-    create unique index ID_Gestionnaire_Aerodrome_IND on Gestionnaire_Aerodrome (id_gestionnaire);
-    create index FKAuthentifier_Gestionnaire_IND on Gestionnaire_Aerodrome (identifiant);
-    create unique index ID_Hangar_IND on Hangar (id_hangar);
-    create unique index ID_Message_IND on Message (id_agent, id_pilote);
-    create index FKMes_Pil_IND on Message (id_pilote);
-    create unique index ID_Parking_IND on Parking (id_parking);
-    create unique index ID_Pilote_IND on Pilote (id_pilote);
-    create index FKAuthentifier_Pilote_IND on Pilote (identifiant);
-    create unique index FKRem_Res_IND on Remplir (id_reservation);
-    create index FKRem_Car_IND on Remplir (Type_carburant);
-    create unique index ID_Reservation_IND on Reservation (id_reservation);
-    create index FKReserver_IND on Reservation (id_vol);
-    create index FKConcerner_IND on Reservation (id_avion);
-    create index FKAllouer_IND on Reservation (id_parking);
-    create index FKAjouter_IND on Reservation (id_facture);
-    create unique index ID_Vol_IND on Vol (id_vol);
 """)
 
 con.commit()
